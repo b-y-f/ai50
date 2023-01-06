ff24aace9b453d2470bf1949f9ed9a9619d93bf8
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-06 14:02:09 +1300

    done node consistency

--- proj3/crossword/generate.py
+++ proj3/crossword/generate.py
@@ -99,7 +99,10 @@ class CrosswordCreator():
         (Remove any values that are inconsistent with a variable's unary
          constraints; in this case, the length of the word.)
         """
-        raise NotImplementedError
+        for var, words in self.domains.items():
+            for word in words.copy():
+                if var.length != len(word):
+                    self.domains[var].remove(word)
 
     def revise(self, x, y):
         """
